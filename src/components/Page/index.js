4a164dfbd80c3e8a3e10632b22321138301bdc88
Vinícius Negrão <vinicius@papricacomunicacao.com.br>
2018-12-18 22:29:53 -0200

    [ * ][ Page ] Added minimal height of 100%

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -7,4 +7,5 @@ export default styled.div`
   background: linear-gradient(135deg, ${props => props.theme.color1}, ${props => props.theme.color2}); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
   width: 100%;
+  min-height: 100%;
 `;
